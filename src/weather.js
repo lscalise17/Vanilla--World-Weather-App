c8c9223e4952db6f284e98d9f0b50a6a1c77eade
lscalise17 <97422956+lscalise17@users.noreply.github.com>
2022-02-25 23:26:02 -0600

    fixed js weather calls

--- src/weather.js
+++ src/weather.js
@@ -28,6 +28,12 @@ function showTemperature(response) {
   currentCity.innerHTML = `${location}`;
   let currentTemp = document.querySelector("#temp");
   currentTemp.innerHTML = `${temperature}`;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 function search(event) {
@@ -36,23 +42,11 @@ function search(event) {
   let city = document.querySelector("#city");
   city.innerHTML = `${searchInput.value}`;
   let units = "metric";
-  let apiKey = "17562d89136e9fe34f60124e09a6da5c";
+  let apiKey = "7df4cedb86cc8633158337122115a5f8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=${units}&appid=${apiKey}`;
 
   axios.get(`${apiUrl}`).then(showTemperature);
 }
-function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "metric";
-  let apiKeyT = "26d3f970bdc8ef4b82b8b13455da9e1a";
-  let apiUrlT = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKeyT}&units=${units}`;
-  axios.get(`${apiUrlT}`).then(showTemperature);
-}
-navigator.geolocation.getCurrentPosition(showPosition);
-
-let button = document.querySelector("#current-location-button");
-button.addEventListener("Click", showPosition);
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
