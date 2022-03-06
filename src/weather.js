b2526a286a1a52588c9c7567ecd0dc7c7ec6726c
lscalise17 <97422956+lscalise17@users.noreply.github.com>
2022-03-06 00:00:51 -0800

    added apiKey and Url for coordinates

--- src/weather.js
+++ src/weather.js
@@ -40,6 +40,8 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function displayForecast() {
@@ -68,6 +70,13 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
+function getForecast(coordinates) {
+  let apiKey = "7df4cedb86cc8633158337122115a5f8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function search(city) {
   let units = "metric";
   let apiKey = "7df4cedb86cc8633158337122115a5f8";
@@ -110,4 +119,3 @@ let celsiuslink = document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
 search("Chicago");
-displayForecast();
